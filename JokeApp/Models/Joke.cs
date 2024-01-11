6a1eac4c143a9b7f5c8e53796ccb00c7275b2684
Doris <107772913+Doris-Siu@users.noreply.github.com>
2024-01-11 23:14:45 +0000

    create a domain Model - Joke

---
+++ JokeApp/Models/Joke.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace JokeApp.Models
+{
+	public class Joke
+	{
+
+		public int Id { get; set; }
+		public string Question { get; set; }
+		public string Answer { get; set; }
+
+		public Joke()
+		{
+		}
+	}
+}
+
