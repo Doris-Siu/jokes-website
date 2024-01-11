ccc3d9054e659580e19fffd309e2e0fcc02fb5b0
Doris <107772913+Doris-Siu@users.noreply.github.com>
2024-01-11 23:27:10 +0000

    create JokesController with 5 View webpages

--- JokeApp/Data/ApplicationDbContext.cs
+++ JokeApp/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using JokeApp.Models;
 
 namespace JokeApp.Data;
 
@@ -9,5 +10,6 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+    public DbSet<JokeApp.Models.Joke> Joke { get; set; } = default!;
 }
 
